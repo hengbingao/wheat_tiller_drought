ea5f096672139cb9b19e9bf37c1d3c2903aa4633
hengbingao <75200685+hengbingao@users.noreply.github.com>
2025-11-20 16:56:38 +0800

    Update peak_annotation_motif_enrichment.sh

--- ATAC-seq/peak_annotation_motif_enrichment.sh
+++ ATAC-seq/peak_annotation_motif_enrichment.sh
@@ -32,7 +32,7 @@ do
     bedtools shuffle \
         -i ${i}.bed \
         -g /scratch/hbgao/wheat-ref/Ta.chrom.size \
-        -incl /scratch/hbgao/ATAC_all/Tn5/DEseq2/diff_peak/sizefactor/final_peak/merged_peak_delMYBS.bed \
+        -incl /scratch/hbgao/ATAC_all/Tn5/DEseq2/diff_peak/sizefactor/final_peak/merged_peak.bed \
         > ${i}.control.inACR.bed
 done
 
@@ -76,50 +76,7 @@ ls *.txt | sed 's/.txt//' | tr '\n' '\t' > header.txt
 
 
 ########################################
-# 5. FIMO on regions excluding ACRs
-########################################
-FIMO_NOACR=/scratch/hbgao/ATAC_all/Tn5/DEseq2/diff_peak/sizefactor/final_peak/Kmeans/fimo_noACR
-
-for i in $(ls *.bed | sed 's/.bed//')
-do
-    bedtools shuffle \
-        -i ${i}.bed \
-        -g /scratch/hbgao/wheat-ref/Ta.chrom.size \
-        -excl /scratch/hbgao/ATAC_all/Tn5/DEseq2/diff_peak/sizefactor/final_peak/merged_peak.bed \
-        > ${i}.control.noACR.bed
-done
-
-for i in $(ls *.bed | sed 's/.bed//')
-do
-    bedtools getfasta \
-        -fi /scratch/hbgao/wheat-ref/Ta.fa \
-        -bed ${i}.bed \
-        > ${i}.fa
-
-    fimo --oc ${i} \
-        /scratch/hbgao/wheat-ref/jasper_meme/root_cluster.meme \
-        ${i}.fa
-done
-
-for i in $(ls *.bed | sed 's/.bed//')
-do
-    cd ${FIMO_NOACR}/${i}
-
-    cut -f1 fimo.tsv \
-        | sort \
-        | uniq -c \
-        | grep 'cluster' \
-        | grep -v 'fimo' \
-        > ${FIMO_NOACR}/${i}.txt
-done
-
-cd ${FIMO_NOACR}
-paste *.txt > all_fimo.txt
-ls *.txt | sed 's/.txt//' | tr '\n' '\t' > header.txt
-
-
-########################################
-# 6. Subgenome distribution of differential peaks
+# 5. Subgenome distribution of differential peaks
 ########################################
 for i in $(ls *.bed | sed 's/.bed//')
 do
@@ -132,7 +89,7 @@ done
 
 
 ########################################
-# 7. Prepare BED file with peak lengths
+# 6. Prepare BED file with peak lengths
 ########################################
 for i in $(ls *.bed | sed 's/.bed//')
 do
